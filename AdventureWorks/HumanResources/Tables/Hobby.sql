fa04a8cbf3808826bce9f66f76f7b17f54b0b5c0
Hannah Gray <hannah.gray@xero.com>
2014-09-12 00:41:59 +1200

    Initial commit of AdventureWorks database into source control

---
+++ AdventureWorks/HumanResources/Tables/Hobby.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [HumanResources].[Hobby] (
+    [HobbyId] INT           IDENTITY (1, 1) NOT NULL,
+    [Hobby]   NVARCHAR (50) NOT NULL,
+    [Active]  BIT           CONSTRAINT [DF_Hobby_Active] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_Hobby] PRIMARY KEY CLUSTERED ([HobbyId] ASC)
+);
+
